d408d1ce0b0983d3451de0077008a26cb51abdd1
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-04-18 13:11:28 +1000

    fuzzy find exclude rules

--- lua/jacob/plugins/telescope.lua
+++ lua/jacob/plugins/telescope.lua
@@ -9,10 +9,20 @@ return {
 		telescope.setup {
 			-- extra setup options, integrate with other plugins etc	
 		}
+
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+
+		local function customFindFiles()
+			builtin.find_files({
+				find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+				previewer = false,
+			})
+		end
+
+		vim.keymap.set("n", "<leader>f", customFindFiles, {})
 		-- <C-f> needs ripgrep
 		vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+		vim.keymap.set("n", "<C-k>", "<C-p>")
 	end
 }
