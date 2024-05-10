0d84193ee2d8cf49573ddfbd6ca83d9abba0ca77
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-05-10 14:12:17 +1000

    chore: tidy up buffer line to remove old theme extension

--- lua/jacob/plugins/bufferline.lua
+++ lua/jacob/plugins/bufferline.lua
@@ -1,34 +1,20 @@
 return {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    -- dependencies only for this catppuccin theme, these will be loaded before plugin is loaded
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-        "catppuccin/nvim",
-    },
-    event = {
-        "VeryLazy",
-    },
-    after = "catppuccin",
-    keys = {
-        { "<C-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-        { "<C-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    },
-    config = function ()
-        require("bufferline").setup {
-            highlights = require("catppuccin.groups.integrations.bufferline").get(),
-            options = {
-                mode = "buffers",
-                separator_style = "slant",
-                show_buffer_icons = true,
-                show_tab_indicators = true,
-                show_close_icon = false,
-                show_buffer_close_icons = false,
-                always_show_bufferline = false,
-                indicator = {
-                    style = "underline",
-                },
-            },
-        }
-    end
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = {
+		"VeryLazy",
+	},
+	keys = {
+		{ "<C-h>",  "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+		{ "<C-l>",  "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+
+	},
+	config = function()
+		require("bufferline").setup {
+		
+		}
+	end
 }
