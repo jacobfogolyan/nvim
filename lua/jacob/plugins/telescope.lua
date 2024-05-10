677f649eeeaa0e40b0f67c963e6e3444f58d0024
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-05-10 14:13:38 +1000

    chore: vertical split of telescope

--- lua/jacob/plugins/telescope.lua
+++ lua/jacob/plugins/telescope.lua
@@ -23,8 +23,17 @@ return {
 						["<C-t>"] = trouble.open_with_trouble,
 						["q"] = actions.close,
 					},
+				},
+				layout_strategy = "vertical",
+				layout_config = {
+					vertical = {
+						preview_height = 0.5,
+						results_height = 0.5,
+					},
+					height = 0.9,
+					width = 0.9,
 				}
-			}
+			},
 		}
 
 		local function customFindFiles()
@@ -34,16 +43,19 @@ return {
 			})
 		end
 
-        -- Open telescope to neovim configuration directory.
-        -- Autocmd must start with upper case letter.
-        vim.api.nvim_create_user_command("Config", function ()
-            builtin.find_files({cwd="~/.config/nvim"})
-        end, {})
+		-- Open telescope to neovim configuration directory.
+		-- Autocmd must start with upper case letter.
+		vim.api.nvim_create_user_command("Config", function()
+			builtin.find_files({ cwd = "~/.config/nvim" })
+		end, {})
 
 		vim.keymap.set("n", "<leader>f", customFindFiles, {})
 		-- vim.keymap.set("n", "C-q", actions.smart_send_to_qflist + actions.open_qflist, {})
-		vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>F", builtin.grep_string, {})
+		-- list buffers
 		vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+		-- Registers
+		vim.keymap.set("n", "<leader>r", builtin.registers, {})
 		vim.keymap.set("n", "<C-k>", "<C-p>")
 	end
 }
