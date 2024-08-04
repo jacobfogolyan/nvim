554412aa1e772a76611d669beb194a20d4384a9f
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2024-08-04 20:33:07 +1000

    chore(theme): change colour

--- lua/jacob/plugins/colors.lua
+++ lua/jacob/plugins/colors.lua
@@ -9,26 +9,26 @@ return {
 	--		vim.cmd.colorscheme "catppuccin"
 	--	end
 	--},
+	{
+		"gmr458/vscode_modern_theme.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("vscode_modern").setup({
+				cursorline = true,
+				transparent_background = false,
+				nvim_tree_darker = true,
+			})
+			vim.cmd.colorscheme("vscode_modern")
+		end,
+	}
+	-- lazy
 	--{
-	--	"gmr458/vscode_modern_theme.nvim",
-	--	lazy = false,
-	--	priority = 1000,
-	--	config = function()
-	--		require("vscode_modern").setup({
-	--			cursorline = true,
-	--			transparent_background = false,
-	--			nvim_tree_darker = true,
-	--		})
-	--		vim.cmd.colorscheme("vscode_modern")
-	--	end,
+	--    "sontungexpt/witch",
+	--    priority = 1000,
+	--    lazy = false,
+	--    config = function(_, opts)
+	--        require("witch").setup(opts)
+	--    end,
 	--}
-    -- lazy
-    {
-        "sontungexpt/witch",
-        priority = 1000,
-        lazy = false,
-        config = function(_, opts)
-            require("witch").setup(opts)
-        end,
-    }
 }
