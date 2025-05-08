1f43589852e1ff816edc3e2e40b2150056a0a094
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2025-05-08 10:26:25 +1000

    chore(telescope): open all files in telescope window

--- lua/jacob/plugins/telescope.lua
+++ lua/jacob/plugins/telescope.lua
@@ -13,16 +13,37 @@ return {
 		local builtin = require("telescope.builtin")
 		local open_with_trouble = require("trouble.sources.telescope").open
 
+		local select_one_or_multi = function(prompt_bufnr)
+			local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+			local multi = picker:get_multi_selection()
+			if not vim.tbl_isempty(multi) then
+				require('telescope.actions').close(prompt_bufnr)
+				for _, j in pairs(multi) do
+					if j.path ~= nil then
+						vim.cmd(string.format('%s %s', 'edit', j.path))
+					end
+				end
+			else
+				require('telescope.actions').select_default(prompt_bufnr)
+			end
+		end
+
+
 		telescope.setup {
 			defaults = {
 				mappings = {
 					i = {
 						["<C-t>"] = open_with_trouble,
+						["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+						["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+						['<CR>'] = select_one_or_multi,
 					},
 					n = {
 						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 						["<C-t>"] = open_with_trouble,
 						["q"] = actions.close,
+						["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+						["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
 					},
 				},
 				layout_strategy = "vertical",
