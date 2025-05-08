9863647772cd0559f4e5f9b04384276b7e3f2c27
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2025-05-08 10:27:22 +1000

    chore(markdown-preview): update

--- lua/jacob/plugins/markdown-preview.lua
+++ lua/jacob/plugins/markdown-preview.lua
@@ -1,9 +1,8 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && yarn install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = true,
+    ft = {
+        "markdown",
+        "codecompanion"
+    }
 }
